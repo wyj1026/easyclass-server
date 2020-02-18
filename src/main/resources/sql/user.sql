e6e0c10a5411e8205f3902824d5597db26dd6be4
wangyijie <wangyijieim@outlook.com>
2020-02-18 18:07:06 +0800

    sql

--- src/main/resources/sql/user.sql
+++ src/main/resources/sql/user.sql
@@ -3,10 +3,10 @@ drop table user;
 CREATE TABLE user
 (
   `id`         int(11)       AUTO_INCREMENT,
-  `name`       varchar(1024) default null ,
-  `nickname`       varchar(1024) default null ,
+  `name`       varchar(1024) default '' ,
+  `nickname`       varchar(1024) default '' ,
   `password`   varchar(1024) NOT NULL,
-  `phone`      varchar(20)   ,
+  `phone`      varchar(20)   default  '',
   `email`   varchar(1024) NOT NULL,
   `avatar_url` varchar(1024) default 'default_avatar_url.jpg',
 
